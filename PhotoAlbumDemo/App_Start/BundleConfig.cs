3c9f8204664604cb7c3a8ea3c32110371beb37e5
Maksim Paulouski <maksim.paulouski@gmail.com>
2017-09-02 19:03:18 +0300

    edit the _BasicLayout

---
+++ PhotoAlbumDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PhotoAlbumDemo.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",
+                                                                  "~/Content/font-awesome.css",
+                                                                  "~/Content/album.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/holder").Include("~/Scripts/holder.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
