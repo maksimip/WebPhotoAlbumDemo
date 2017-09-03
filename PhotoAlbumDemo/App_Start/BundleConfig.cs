e273a36347107ebf1f70b2dc249717cb86f26313
Maksim Paulouski <maksim.paulouski@gmail.com>
2017-09-03 15:08:16 +0300

    create page of authorization

--- PhotoAlbumDemo/App_Start/BundleConfig.cs
+++ PhotoAlbumDemo/App_Start/BundleConfig.cs
@@ -14,11 +14,14 @@ namespace PhotoAlbumDemo.App_Start
                                                                   "~/Content/font-awesome.css",
                                                                   "~/Content/album.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-3.1.1.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include("~/Scripts/umd/popper.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/holder").Include("~/Scripts/holder.js"));
+            
 
             BundleTable.EnableOptimizations = true;
         }
